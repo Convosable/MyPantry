fd67a2d55f6e28a082cd322cf2bc150e932c01df
Connor Vosberg <connorjv@gmail.com>
2023-03-20 16:12:10 -0700

    fetches ingredient info on page load and renders each ingredient to MyPantry

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import Home from './Home';
 import NavBar from './NavBar';
 import MyRecipes from './MyRecipes';
@@ -6,7 +7,19 @@ import CreateRecipe from './CreateRecipe';
 import MyPantry from './MyPantry';
 import { Route, Switch } from "react-router-dom"
 
+
 function App() {
+
+  const [ingredientList, setIngredientList] = useState([])
+
+  const ingredientsData = `http://localhost:3000/ingredients`
+  // const recipesData = `http://localhost:3000/recipe`
+
+  useEffect(() => {
+    fetch(ingredientsData)
+    .then(r=>r.json())
+    .then(ingredient => setIngredientList(ingredient))
+  },[]);
 
   return (
     <div className="App">
@@ -19,7 +32,7 @@ function App() {
           <MyRecipes />
         </Route>
         <Route exact path = "/mypantry">
-          <MyPantry />
+          <MyPantry ingredientList = {ingredientList}/>
         </Route>
         <Route exact path = "/createrecipe">
           <CreateRecipe />
@@ -30,3 +43,11 @@ function App() {
 }
 
 export default App;
+
+//App
+    //Home
+    //NavBar
+    //MyPantry
+    //MyRecipes
+              //Recipe Card
+    //CreateRecipe
--- src/MyPantry.js
+++ src/MyPantry.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-function MyPantry() {
+function MyPantry({ingredientList}) {
+    
+    console.log(ingredientList);
 
     return (
         <div>
             <h1>My Pantry</h1>
             <div>
                 <h2>Ingrediants</h2>
+                {ingredientList.map((ingredient) => 
+                <div key = {ingredient.name}>
+                    <h2>Name: {ingredient.name}</h2>
+                    <h3>Category: {ingredient.category}</h3>
+                </div>
+                )}
                 <p>Search by name:</p>
+                
             </div>
             <div>
                 <h2>My Ingrediants</h2>
@@ -18,3 +27,6 @@ function MyPantry() {
 }
 
 export default MyPantry;
+
+
+//create an Ingredient Card component for styling purposes
