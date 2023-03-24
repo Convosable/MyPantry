e165b6adda849a27ec7c2bdea09f2719995aeffd
Connor Vosberg <connorjv@gmail.com>
2023-03-24 14:45:30 -0700

    updates recipeCard to show all necessary info

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -2,11 +2,21 @@ import React from "react";
 
 function RecipeCard({recipe}) {
 
-    const {name, type} = recipe;
+    const {name, image, type, preptime, cooktime, ingredients, instructions} = recipe;
     return (
-        <div>
+        <div className = "recipecard">
             <h1>{name}</h1>
+            <img src = {image} alt = {name}/>
             <h2>{type}</h2>
+            <p>Preptime: {preptime}</p>
+            <p>Cooktime: {cooktime}</p>
+            <p>Ingredients: {ingredients.map((ingr) => <li>{ingr}</li>)}</p>
+            <div className = "instructions">
+                <p>Instructions:</p>
+                <ol className = "list" type="1">
+                    {instructions.map((inst) => <li>{inst}</li>)}
+                </ol>
+            </div>
         </div>
     )
 }
